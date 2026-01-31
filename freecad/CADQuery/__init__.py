4a9c768a228172f4933e69e50a01c6f1bcee68e1
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-31 16:43:29 -0500

    Fix imports

--- freecad/CADQuery/__init__.py
+++ freecad/CADQuery/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-FileNotice: Part of the CADQuery addon.
 
 
-from CQGui.display import show_object
+from .display import show_object
 
 # Register the show_object function as a global function
 globals()['show_object'] = show_object
